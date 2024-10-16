410ce139e3cfa3eef62e5df66cfff028111ef21e
Tendai2630 <tindoza@gmail.com>
2024-10-16 17:55:58 +0200

    addes axios

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
-function handleSearchSubmit(event){
-    event.preventDefault();
-    let searchInput= document.querySelector("#search-form-input");
-    let cityElement =document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+function refreshweather(response){
+    let temperatureElement =document.querySelector("#temperature");
+    let temperature =response.data.temperature.current;
+    let cityElement= document.querySelector("#city")
+   
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = math.round(temperature);
+
+}
+function searchcity(city){
+    let apikey= "t4f81808b6o8c140994f8e9749ba33a0";
+     let apiUrl="https://api.shecodes.io/weather/v1/current?query=${city}&key=${api}&unts=metric"; 
+    axios.get(apiUrl).then(refreshweather);
 }
 
 
 
+function handleSearchSubmit(event){
+    event.preventDefault();
+    let searchInput= document.querySelector("#search-form-input");
+    searchCity(searchInput.value);
+
+}
 
 
 let seachformElement=document.querySelector("#search-form");
 seachformElement.addEventListener("submit",handleSearchSubmit);
+
+searchCity("PARIS");
