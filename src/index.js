a60b1def53cbc7c8c6aeedcfff7113302af2d725
Tendai2630 <tindoza@gmail.com>
2024-10-17 18:59:49 +0200

    added html

--- src/index.js
+++ src/index.js
@@ -53,8 +53,35 @@ function refreshWeather(response) {
   
     searchCity(searchInput.value);
   }
-  
-  let searchFormElement = document.querySelector("#search-form");
-  searchFormElement.addEventListener("submit", handleSearchSubmit);
-  
-  searchCity("Paris");
+  function displayforecast()
+
+let days= ["mon" ,"tue", "wed", "thurs", "fri"]
+let forecastHtml="";
+
+days.forEach(function(day)){
+  forecastHtml=
+  forecastHtml +
+
+  "<div class="weather-forecast>
+      <div class="weather-forecast-day">
+          <div class="weather-forecast-date">${day}</div>
+          <div class="weather-forecast-icon">🌥️</div>
+          <div class="weather-forecast-temperature"><strong>15°</strong>
+               9°</div>  
+</div>
+";})";
+
+let forecastElement=document.querySelector("#forecast");
+forecastElement.innerHTML= forecastHtml;
+
+
+}
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Paris");
+
+ diplay ("#forecast");
+
+
+ 
